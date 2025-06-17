4e1601f70491a927c7db8daa136bca66811249d6
dhiraj <dhiraj@opspl.com>
2025-06-17 10:31:49 +0530

    Fix: Adjust paths for Vercel deployment

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/', // Required for Vercel routing
+    publicPath: './', // Required for Vercel routing
   },  
   module: {
     rules: [
